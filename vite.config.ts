661a15041d5a7af00edde28dd51bb5af43135cce
credit23 <anisakb20@gmail.com>
2025-05-30 01:43:51 +0100

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// replace with your repo name
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: '/gradewebsite/',
+  plugins: [react()]
+})
